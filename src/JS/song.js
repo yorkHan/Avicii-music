bf980185743bfc49059a0b6560ea95ccadf7d079
yorkHan <393900482@qq.com>
2018-05-30 17:38:52 +0800

    修改autoplay在安卓浏览器的bug

--- src/JS/song.js
+++ src/JS/song.js
@@ -16,18 +16,31 @@
             this.view.init()
             this.model=model  
             this.bindEvents()  
+            this.musicPause()
+            this.musicEnd()
         },
         bindEvents(){
             this.view.$el.click(()=>{
                 this.view.$el.find('.cover').toggleClass('active')
                 this.view.$el.find('.icon').toggleClass('active')
-                let audio=$("#myAudio")[0]
-                if(audio.paused)                     {                 
-                    audio.play();//audio.play();// 这个就是播放  
-                }else{
-                 audio.pause();// 这个就是暂停
-                }
             })
+        },
+        musicPause(){
+            this.view.$el.click(()=>{
+            let audio=$("#myAudio")[0]
+            if(audio.paused)                     {                 
+                audio.play();//audio.play();// 这个就是播放  
+            }else{
+             audio.pause();// 这个就是暂停
+            }
+        })
+        },
+        musicEnd(){
+            let audio=$("#myAudio")[0]
+            audio.onended = ()=>{
+                this.view.$el.find('.cover').addClass('active')
+                this.view.$el.find('.icon').addClass('active')
+            };
         }
     }
     controller.init(view,model)
